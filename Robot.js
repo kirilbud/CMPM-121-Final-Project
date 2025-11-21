710d88bd95d712330ecf9d08df3fe0bca69b926a
kirilbud <kiril.saltz@gmail.com>
2025-11-21 12:15:36 -0800

    updated robot

--- Robot.js
+++ Robot.js
@@ -2,7 +2,43 @@ import * as THREE from 'three';
 import { GLTFLoader } from './lib/addons/GLTFLoader.js';
 
 export class Robot {
-    constructor(scene){
-        
+    constructor(scene, position){
+        const scale = .6;
+
+        //robot variables
+        this.up = new THREE.Vector3(0,1,0);
+        this.forward = new THREE.Vector3(1,0,0); //the direction the bot is walking
+        this.speed = 1;
+        this.animations = null; //this holds the animations of the bot
+        this.mixer; //animation mixer for the bot
+        this.bot; //the robots mesh
+
+        const gltfLoader = new GLTFLoader();
+        let url = "./glb/UV_the_robot.glb";
+        gltfLoader.load(url, (gltf) => {
+
+            const root = gltf.scene;
+            root.scale.set(scale, scale, scale);
+            root.position.set(position);
+            scene.add(root);
+
+
+            this.mixer = new THREE.AnimationMixer(root);
+            this.bot = root;
+
+            console.log(this.bot)
+            const clips = gltf.animations;
+
+            //add all animations to a dictionary
+            this.animations = clips;
+
+            const clip = THREE.AnimationClip.findByName(clips, 'Run'); //find the run animation
+            const action = this.mixer.clipAction(clip);
+            action.play();
+        });
+    }
+
+    Update() {
+
     }
 }
