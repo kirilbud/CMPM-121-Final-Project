2572916e88075617a79ed46f9fefba7ac8c21f15
kirilbud <kiril.saltz@gmail.com>
2025-12-05 00:23:52 -0800

    level loading

--- worldObjectClasses/Finish.js
+++ worldObjectClasses/Finish.js
@@ -4,12 +4,15 @@ import { GLTFLoader } from '../lib/addons/GLTFLoader.js'
 import * as CANNON from 'https://unpkg.com/cannon-es@0.19.0/dist/cannon-es.js'
 import { PhysicsObject } from './PhysicsObject.js'
 
+import { Level_1 } from '../WorldData.js'
+
 //to be implemented
 export class Finish extends WorldObject {
     constructor(scene, cannon, position, level) {
         super(scene, cannon, position)
         this.robots_till_win = 3
         this.setColor(0xffffff)
+        this.level = level
     }
     interact(robot) {
         //replace with a jumpscare or something idk anymore
@@ -25,6 +28,7 @@ export class Finish extends WorldObject {
         if (this.robots_till_win == 0) {
             //alert('brace make me a sandwich')
             //window.location.reload()
+            this.level.loadNewLevel(Level_1)
         }
     }
 }
